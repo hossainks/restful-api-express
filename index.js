fd17ca61ab2d3f83ebdf36361bb07c445faeb108
hossainks <shakirhossain.qa@gmail.com>
2024-12-25 20:00:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Description: This file is the entry point of the application.
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
