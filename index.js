7a3660e63f88c3603059195d0f2f5daa026cd349
hossainks <shakirhossain.qa@gmail.com>
2024-12-25 20:10:24 +0600

    Add dynamic ports

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server is running on port 3000"));
