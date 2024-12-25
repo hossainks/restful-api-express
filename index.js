8331c9645e6a95d81bcecd0adc2a130652e2845b
hossainks <shakirhossain.qa@gmail.com>
2024-12-25 20:12:11 +0600

    Update server running text

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.get("/api/courses", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Server is running on port 3000"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
